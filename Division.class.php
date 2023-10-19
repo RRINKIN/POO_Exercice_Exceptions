378c9c2cbe65e62fe731ed4cd9645e577c6c3e51
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2023-10-19 18:27:59 +0200

    correcting error display and order of ifs

--- Division.class.php
+++ Division.class.php
@@ -1,6 +1,10 @@
 <?php
 class Division extends Operation {
     public function operate(){
+        // controle argument vide
+        if (empty($this->nombre1) || empty($this->nombre2)) {
+            throw new InvalidArgumentException("Merci de renseigner une valeur");
+        }
         // controle de la valeur numérique
         if (!is_numeric($this->nombre1) || !is_numeric($this->nombre2)) {
             throw new InvalidArgumentException("La valeur doit être un nombre");
@@ -8,10 +12,6 @@ class Division extends Operation {
         // controle de la division par 0
         if ($this->nombre2 === 0) {
             throw new InvalidArgumentException("La division par 0 est impossible");
-        }
-        // controle argument vide
-        if (empty($this->nombre1) || empty($this->nombre2)) {
-            throw new InvalidArgumentException("Merci de renseigner une valeur");
         }
         // si tout se passe bien
         return $this->nombre1 / $this->nombre2;
--- index.php
+++ index.php
@@ -7,42 +7,42 @@ spl_autoload_register('loadClass');
 
 // call Somme with catch exceptions
 try {
-    $newSum = new Somme(1, 4);
+    $newSum = new Somme(1, "a");
     $somme = $newSum->operate();
+    echo $somme;
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-echo "$somme";
 echo "<br>";
 
 // call Soustraction
 try {
     $newSoustraction = new Soustraction(6,1);
     $soustraction = $newSoustraction->operate();
+    echo "$soustraction";
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-echo "$soustraction";
 echo "<br>";
 
 // call Multiplication
 try {
     $newMultiplication = new Multiplication(2.5,2);
     $multiplication = $newMultiplication->operate();
+    echo "$multiplication";
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-echo "$multiplication";
 echo "<br>";
 
 // call Division
 try {
-    $newDivision = new Division(10,0);
+    $newDivision = new Division(10,"");
     $division = $newDivision->operate();
+    echo "$division";
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-echo "$division";
 echo "<br>";
 
 ?>
